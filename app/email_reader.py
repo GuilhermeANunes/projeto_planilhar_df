3b92d63b793f811aa25174275221a0751a163f1a
Guilherme Nunes <gui_augustonunes@outlook.com>
2026-06-11 21:57:06 -0300

    Adjusting code details of the email_reader

--- app/email_reader.py
+++ app/email_reader.py
@@ -2,12 +2,17 @@ import win32com.client #type: ignore
 
 
 class EmailService:
+    
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
     inbox = outlook.GetDefaultFolder(6)  # 6 é a pasta de entrada
     messages = inbox.Items
 
     for message in list(messages):
-        if message.Subject == "PLANILHAR":
+        if message.Subject == "PLANILHAR" and message.UnRead:
+            print(f'Mensagem de: {message.SenderEmailAddress}')
+            print(f"Assunto: {message.Subject}")
+            remetente = message.SenderEmailAddress
+            message.UnRead = False  # Marcar como lida
             attachments = message.Attachments
             for attachment in attachments:
                 attachment.SaveAsFile(f"C:\\Users\\gui_a\\OneDrive\\Área de Trabalho\\projeto_planilhar_df\\assets\\{attachment.FileName}")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv  # type: ignore
 from app.email_reader import EmailService
+import time
 
 # Carregando as credencias do arquivo .env
 load_dotenv()
@@ -8,4 +9,7 @@ def main():
     EmailService()
     
 if __name__ == "__main__":
-    main()
+
+    while True:
+        main()
+        time.sleep(10)  # Espera 60 segundos antes de verificar novamente
